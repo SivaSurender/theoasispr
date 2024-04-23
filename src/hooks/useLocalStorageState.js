7068dbe99e19ad7725d0728d1f8da06a89798c2c
SivaSurender <e.surender14@gmail.com>
2024-04-23 20:41:00 +0530

    added type checking with refactor

--- src/hooks/useLocalStorageState.js
+++ src/hooks/useLocalStorageState.js
@@ -1,17 +1,33 @@
 import { useState, useEffect } from "react";
 
+/**
+ *
+ * @template T
+ * @param {T} initialState - Initial state value.
+ * @param {string} key - Key to identify the localStorage entry.
+ * @returns {[T, React.Dispatch<React.SetStateAction<T>>]} - Tuple containing state value and setter function.
+ */
 export function useLocalStorageState(initialState, key) {
-  const [value, setValue] = useState(function () {
-    const storedValue = localStorage.getItem(key);
-    return storedValue ? JSON.parse(storedValue) : initialState;
+  const [value, setValue] = useState(() => {
+    try {
+      const storedValue = localStorage.getItem(key);
+      return storedValue ? JSON.parse(storedValue) : initialState;
+    } catch (error) {
+      console.error(
+        `Error retrieving state from localStorage: ${error.message}`
+      );
+      return initialState;
+    }
   });
 
-  useEffect(
-    function () {
+  // Update localStorage whenever the state changes
+  useEffect(() => {
+    try {
       localStorage.setItem(key, JSON.stringify(value));
-    },
-    [value, key]
-  );
+    } catch (error) {
+      console.error(`Error storing state to localStorage: ${error.message}`);
+    }
+  }, [value, key]);
 
   return [value, setValue];
 }
